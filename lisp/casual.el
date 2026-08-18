32ac5cd19b74bff129ef870d96e7344b4a380dfb
Charles Choi <kickingvegas@gmail.com>
2026-08-17 22:26:34 -0700

    Bump version to 3.0.2-rc.1

--- lisp/casual.el
+++ lisp/casual.el
@@ -5,8 +5,8 @@
 ;; Author: Charles Choi <kickingvegas@gmail.com>
 ;; URL: https://github.com/kickingvegas/casual
 ;; Keywords: tools, wp
-;; Version: 3.0.1
-;; Package-Requires: ((emacs "30.1") (transient "0.9.0") (csv-mode "1.27"))
+;; Version: 3.0.2-rc.1
+;; Package-Requires: ((emacs "29.4") (transient "0.9.0") (csv-mode "1.27"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
